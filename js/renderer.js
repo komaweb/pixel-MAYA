c68a0b2284fff0362871c2ec79158fb4fd263c76
komaweb <marimoxinzhong@gmail.com>
2026-07-06 20:47:17 +0900

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,10 +1,13 @@
 export function drawCharacter(ctx, x, y, size, hat, face){
 
-    ctx.fillStyle = "#ff8080";
+    // 背景色（仮）
+    ctx.fillStyle = "#ff9999";
     ctx.fillRect(x, y, size, size);
 
+    // 被り物
     ctx.drawImage(hat, x, y, size, size);
 
+    // 顔
     ctx.drawImage(face, x, y, size, size);
 
 }
