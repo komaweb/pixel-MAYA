667b27876b63137f23fa492ce616812165429f63
komaweb <marimoxinzhong@gmail.com>
2026-07-06 18:46:13 +0900

    Create imageLoader.js

---
+++ js/imageLoader.js
@@ -0,0 +1,45 @@
+import {CONFIG} from "./config.js";
+
+export function createImageData(img){
+
+    const canvas=document.getElementById("canvas");
+
+    canvas.width=CONFIG.outputWidth;
+
+    canvas.height=CONFIG.outputHeight;
+
+    const ctx=canvas.getContext("2d");
+
+    ctx.drawImage(
+
+        img,
+
+        0,
+
+        0,
+
+        CONFIG.outputWidth,
+
+        CONFIG.outputHeight
+
+    );
+
+    return{
+
+        ctx,
+
+        imageData:ctx.getImageData(
+
+            0,
+
+            0,
+
+            CONFIG.outputWidth,
+
+            CONFIG.outputHeight
+
+        )
+
+    };
+
+}
