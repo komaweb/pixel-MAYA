05d89761379a0aff8d8872d3a7ede719ee84fb53
komaweb <marimoxinzhong@gmail.com>
2026-07-06 18:55:58 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,39 @@
-export function logPixels(imageData){
+import {rgbToHsv} from "./hsv.js";
+import {
+    getHatIndex,
+    getExpressionIndex
+} from "./converter.js";
 
-    console.log(imageData.data);
+export function debugPixels(imageData){
+
+    const data = imageData.data;
+
+    for(let i=0;i<data.length;i+=4){
+
+        const r = data[i];
+        const g = data[i+1];
+        const b = data[i+2];
+
+        const hsv = rgbToHsv(r,g,b);
+
+        const hat = getHatIndex(hsv.h);
+
+        const expression = getExpressionIndex(hsv.v);
+
+        console.log({
+
+            rgb:[r,g,b],
+
+            h:hsv.h.toFixed(1),
+
+            v:hsv.v.toFixed(2),
+
+            hat,
+
+            expression
+
+        });
+
+    }
 
 }
